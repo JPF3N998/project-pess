4ea614f735ae340c089fc0cd89595908cc61fb7f
Jose Pablo Feng <jpfeng-dev@proton.me>
2024-02-15 16:31:57 -0600

    chore: Added ts-standard

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+
+const app = express()
+
+app.use(morgan('tiny'))
+app.use(helmet())
+
+const port = process.env.PORT ?? 3000
+
+app.listen(port, () => {
+  console.log('Listening on', port)
+})
